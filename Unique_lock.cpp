3daf11e02b1d0dd62c7468a2d97d82b862fb5657
TheDonten <32191011+TheDonten@users.noreply.github.com>
2019-02-03 00:57:18 +0300

    Create Unique_lock.cpp

---
+++ Unique_lock.cpp
@@ -0,0 +1,84 @@
+#include <mutex>
+template <class Mutex>
+class unique_lock
+{
+	Mutex *m_;
+	bool is_lock;
+public:
+	unique_lock() 
+		: m_(nullptr), is_lock(false)
+	{}
+
+	~unique_lock()
+	{
+		if (is_lock) 
+			m_->unlock();
+	}
+
+	unique_lock(unique_lock&& other) noexcept : m_(other.m_)
+	{
+		other.m_ = nullptr;
+		is_lock = other.is_lock;
+		other.is_lock = false;
+	}
+
+	explicit unique_lock(Mutex & m) 
+	{
+		m_ = &m;
+		(*m_).lock();
+		is_lock = true;
+	}
+	unique_lock(Mutex& m, std::defer_lock_t t) noexcept 
+	{
+		m_ = &m;
+		is_lock = false;
+	}
+
+	unique_lock(Mutex& m, std::adopt_lock_t t) 
+	{
+		m_ = &m;
+		is_lock = true;
+	}
+
+	void lock()
+	{
+		m_->lock();
+		is_lock = true;
+	}
+
+	void unlock()
+	{
+		if(m_ && is_lock)
+		{
+			m_->unlock();
+			is_lock = false;
+		}
+		//else throw
+	}
+
+	unique_lock<Mutex>& operator=(unique_lock<Mutex>&& other) 
+	{
+		if (this != std::addressof(other))
+			{
+			if (is_lock)
+				m_->unlock();
+			m_ = other.m_;
+			is_lock = other.is_lock;
+			other.m_ = nullptr;
+			other.is_lock = false;
+			}
+		return (*this);
+	}
+};
+
+
+int main()
+{
+	std::mutex m;
+	std::defer_lock_t t;
+	unique_lock<std::mutex> l1(m);
+	
+	unique_lock<std::mutex> l2(std::move(l1));
+
+	return 0;
+}
