d8659ca09482af5b06e2e66158f3f67b83832ed8
TheDonten <32191011+TheDonten@users.noreply.github.com>
2019-02-03 00:07:19 +0300

    Update unique_ptr.cpp

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,80 +1,90 @@
+#include "pch.h"
+#include <iostream>
+#include <atomic>
 template <typename T>
 class unique_ptr {
 	T * ptr_;
 public:
-	unique_ptr() 
-        { 
-		ptr_ = nullptr; 
+	unique_ptr()
+	{
+		ptr_ = nullptr;
+	}
+	
+	explicit unique_ptr(T* ptr) {
+		ptr_ = ptr;
 	}
 	~unique_ptr() {
-		if(ptr_) delete ptr_; 
+		if (ptr_) delete ptr_;
 	}
 
 	unique_ptr(unique_ptr<T> && Uptr)
-	{ 
-		swap(Uptr.ptr_, ptr_); 
+	{
+		std::swap(Uptr.ptr_, ptr_);
 	}
 
 	unique_ptr<T> & operator=(unique_ptr<T> && Uptr)
-	{ 
+	{
 		if (this != std::adressof(Uptr))
-			swap(Uptr.ptr_, ptr_);
+			std::swap(Uptr.ptr_);
 		return *this;
 	}
 
-	void reset(T * ptr = nullptr) 
-	{ 
-		ptr_ = ptr; 
+	void reset(T * ptr = nullptr)
+	{
+		ptr_ = ptr;
 	}
 
-	T * release() 
+	T * release()
 	{
 		T * pointer = ptr_;
 		ptr_ = nullptr;
 		return pointer;
 	}
-	void swap(unique_ptr<T> &other) 
-	{ 
-		std::swap(other.ptr_, ptr_); 
+	void swap(unique_ptr<T> &other)
+	{
+		std::swap(other.ptr_, ptr_);
 	}
-	
+
 	unique_ptr(unique_ptr& ptr) = delete;
 	unique_ptr& operator = (unique_ptr&) = delete;
 
-	T* operator->() 
-	{ 
+	T* operator->()
+	{
 		return ptr_;
 	}
 
-	T & operator * () 
+	T & operator * ()
 	{
-		return *ptr_; 
+		return *ptr_;
 	}
 
-	operator bool() const 
+	operator bool() const
 	{
-		if (ptr_) 
+		if (ptr_)
 			return true;
 		return false;
 	}
 
-	T * get() 
-	{ 
+	T * get()
+	{
 		return ptr_;
 	}
 };
 
-template<typename T, typename... Args> 
-unique_ptr<T> make_unique(Args&&... args) 
-{ 
-	return unique_ptr<T>(new T(std::forward<Args>(args)...)); 
+template<typename T, typename... Args>
+unique_ptr<T> make_unique(Args&&... args)
+{
+	return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 
-int main() 
+int main()
 {
-	int *A = new int(5);
-	unique_ptr<int> ptr;
-	ptr.reset(A);
-	std::cout << *ptr;
+	
+	auto ptr = make_unique<int>(5);
+	unique_ptr<int> ptr2(std::move(ptr));
+	//auto ptr1 = std::move(ptr);
+	
+
+	
 }
