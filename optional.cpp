1601ec2ee3b7105eb29fcb127bcedb5b2bc70901
TheDonten <32191011+TheDonten@users.noreply.github.com>
2019-02-03 21:46:22 +0300

    Update optional.cpp

--- optional.cpp
+++ optional.cpp
@@ -1,85 +1,63 @@
-// something.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
-#include <iostream>
-#include <string>
-class Naxui {
+// не компилируется , писал по стандарту, вообще хз 
+template <class T>
+class my_opt {
 public:
-	Naxui() {
-		heh = nullptr;
+	constexpr my_opt() noexcept
+		:_Dummy{}, ok{ false }
+	{}
+	constexpr my_opt(std::nullopt_t) noexcept:
+		my_opt()
+	{}
+	constexpr my_opt(const my_opt& other) = default;
+	constexpr my_opt(my_opt&& other) noexcept = default;
+	my_opt<T>& operator=(const my_opt<T>& other) = default;
+	my_opt<T>& operator=(my_opt<T>&& other) = default;
+	~my_opt() = default;
+	T& operator*()
+	{
+		return std::addressof(this->_get());
 	}
-
-	Naxui(std::string &&a)  {
-		heh = new daughter_class(a);
+	T* operator->()
+	{
+		return this->_get();
 	}
-	class Base_class {
-
-
-	public:
-		std::string meh;
-		Base_class() {
-			std::cout << "base_class" << std::endl;
-		}
-		Base_class(std::string name) : meh(name) {
-			//meh = name;
-			std::cout << "base_class" << std::endl;
-		}
-		virtual void write(std::string name) {
-			std::cout << name << std::endl;
-		
-		}
-		virtual void out() const {
-			std::cout << meh << std::endl;
-		}
-		//	 virtual void out() const = 0;
-		~Base_class() {
-			std::cout << "delete base_class" << std::endl;
-		}
-	};
-	class daughter_class : public Base_class {
-
-
-	public:
-		std::string meheh;
-		daughter_class() {
-			std::cout << "daughter_class" << std::endl;
-		}
-		daughter_class(std::string name) : Base_class(name)
+	operator bool() const
+	{
+		return ok;
+	}
+	T& value()
+	{
+		if (!ok)
 		{
-			std::cout << "daughter_class" << std::endl;
-		
+			throw(std::bad_optional_access{});
+			// ??
 		}
-		void write(std::string name)  override {
-			std::cout << name << std::endl;
-			//meheh = name;
+		else
+		{
+			return this->_get();
 		}
-		void out() const {
-			std::cout << meheh << std::endl;
+	}
+
+	template <class U>
+	T value_or(U&& default_value)
+	{
+		if (ok)
+			return this->_get;
+		else
+		{
+			return static_cast<T>(std::forward<U>(default_value));
 		}
-		~daughter_class() {
-			std::cout << "delete daughter" << std::endl;
-		}
-	};
+	}
+
 private:
-	Base_class* heh;
+	constexpr T& _get() & noexcept
+	{
+		return (this->_Value);
+	}
+	union
+	{
+		char _Dummy;
+		std::remove_const_t<T> _Value;
+	};
+	bool ok;
 };
-int main()
-{
-	Naxui a("sukas");
-	
-	//std::string name = { "kek" };
-	
-	
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
