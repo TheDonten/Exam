9c4a48efa4c69122fddb736b399bf05567397cbc
TheDonten <32191011+TheDonten@users.noreply.github.com>
2019-02-03 18:27:38 +0300

    Update Safestack.cpp

--- Safestack.cpp
+++ Safestack.cpp
@@ -1,30 +1,41 @@
-template <typename T>
-class SafeStack{
+#include "pch.h"
+#include <mutex>
+#include <stack>
+#include <iostream>
+#include <queue>
+#include <thread>
+
+template <typename t>
+class SafeStack {
 private:
-std::stack<t> data;
-std::mutex mut;
+	std::stack<t> data;
+	mutable std::mutex m;
 public:
 
-Safestack(){};
-Safestack(Safestack & other){
-std::lock_guard<std::mutex>lock(other.m);
-data = other.data;
-}
-Safestack& operator =(Safestack & other) = delete;
-void Pop(){
-std::lock_guard<std::mutex> lock(m);
-	if (data.empty()) throw std::invalid_argument("error");
-	value = std::move(data.top());
-data.pop();
-}
-void push(t new_value)
-{
-	std::lock_guard<std::mutex>lock(m);
-	data.push(std::move(new_value)); 
-}
-bool empty()const
-{
-	std::lock_guard<std::mutex> lock(m);
-	return data.empty();
-}
+	SafeStack() {};
+	SafeStack(SafeStack & other) {
+		std::lock_guard<std::mutex>lock(other.m);
+		data = other.data;
+	}
+	SafeStack& operator =(SafeStack & other) = delete;
+	void Pop() {
+		std::lock_guard<std::mutex> lock(m);
+		if (data.empty()) throw std::invalid_argument("error");
+		//value = std::move(data.top());
+		data.pop();
+	}
+	void push(t new_value)
+	{
+		std::lock_guard<std::mutex>lock(m);
+		data.push(std::move(new_value));
+	}
+	bool empty()const
+	{
+		std::lock_guard<std::mutex> lock(m);
+		return data.empty();
+	}
 };
+int main() {
+	SafeStack<int> data;
+	data.push(5);
+}
