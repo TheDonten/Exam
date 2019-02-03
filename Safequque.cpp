142842bfbf2f4d329bfd3af9bd7234a281196a6f
TheDonten <32191011+TheDonten@users.noreply.github.com>
2019-02-03 18:02:02 +0300

    Update Safequque.cpp

--- Safequque.cpp
+++ Safequque.cpp
@@ -7,18 +7,12 @@ class threadsafe_queue
 {
 private:
 	mutable std::mutex mut;
-	std::queue<std::shared_ptr<t> > data_queue;
+	std::queue<t> data_queue;
 	std::condition_variable data_cond;
 public:
 	threadsafe_queue() {}
-	
-	void wait_and_pop(t& value)
-	{
-		std::unique_lock<std::mutex> lk(mut);
-		data_cond.wait(lk, [this] {return !data_queue.empty(); });
-		value = std::move(*data_queue.front());
-		data_queue.pop();
-	}
+
+
 	bool try_pop(T& value)
 	{
 		std::lock_guard<std::mutex> lk(mut);
@@ -29,34 +23,14 @@ public:
 		return true;
 	}
 
-	std::shared_ptr<t> wait_and_pop()
-	{
-		std::unique_lock<std::mutex> lk(mut);
-		data_cond.wait(lk, [this] {return !data_queue.empty(); });
-		std::shared_ptr<t> res = data_queue.front();
-		data_queue.pop();
-		return res;
-	}
-	std::shared_ptr<t> try_pop()
-	{
-		std::lock_guard<std::mutex> lk(mut);
-		if(data_queue.empty())
-			return std::shared_ptr<t>();
-		std::shared_ptr<t> res = data_queue.front();
-		data_queue.pop();
-		return res;
-	}
+
 	void push(t new_value)
 	{
-		std::shared_ptr<t> data(
-			std::make_shared<t>(std::move(new_value)));
+		
+			
 		std::lock_guard<std::mutex> lk(mut);
-		data_queue.push(data);
-		data_cond.notify_one();
+		data_queue.push(new_value);
+	    
 	}
-	bool empty() const
-	{
-		std::lock_guard<std::mutex> lk(mut);
-		return data_queue.empty();
-	}
+
 };
